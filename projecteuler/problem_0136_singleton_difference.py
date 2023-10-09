72bd1c4b14dd717c1443ad0861a0e19a0c9e64f1
RyoNak-billy <nakagamiryo0901@gmail.com>
2023-10-10 01:22:50 +0900

    project euler 136 singleton difference

---
+++ projecteuler/problem_0136_singleton_difference.py
@@ -0,0 +1,24 @@
+class Solution:
+    """
+    Problem 
+        https://projecteuler.net/problem=136
+        
+    Usage
+        Solver = Solution(50000000)
+        Solver.solve(1)
+    """
+    def __init__(self, max_n):
+        self.max_n = max_n
+
+    def count_solution(self):
+        self.res = [0] * (self.max_n + 1)
+        for y in range(1, self.max_n + 1):
+            for d in range(int((y)/4) + 1, y):
+                n_idx =  y * (4*d - y)
+                if n_idx > self.max_n:
+                    break
+                self.res[n_idx] += 1
+    
+    def solve(self, sol_number):
+        self.count_solution()
+        return self.res.count(sol_number)
