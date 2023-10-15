74d5db172a4b07030d16b4bbaece76375604ac8f
RyoNak-billy <nakagamiryo0901@gmail.com>
2023-10-16 05:44:44 +0900

    1269 number of ways to stay

---
+++ Leetcode/problem_1269_number_of_ways_to_stay.py
@@ -0,0 +1,27 @@
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+        ## reduce the arrLen size
+        arrLen = min(steps // 2 + 1, arrLen)
+        
+        if arrLen == 1:
+            return 1
+        
+        res = [0] * arrLen
+        res[0] = 1
+
+        for d in range(steps):
+            a = res
+            b = [0] * arrLen
+
+            b[0] += a[0]
+            b[1] += a[0]
+            max_idx = min(arrLen, steps+1-d)
+            for i in range(1, max_idx - 1):
+                b[i-1] += a[i]
+                b[i] += a[i]
+                b[i+1] += a[i]
+            b[max_idx-1] += a[max_idx-1]
+            b[max_idx-2] += a[max_idx-1]
+            res = b
+
+        return res[0] % (10**9 + 7)
